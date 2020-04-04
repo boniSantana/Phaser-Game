5f97ad0e970d8d99b00035e6f000f865422f6a1d
Santiago Santana <santana.santiago@gmail.com>
2020-04-04 14:42:18 -0300

    fix webpack config and install deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,51 @@
-'use strict';
+"use strict";
 
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  devtool: "eval-source-map",
 
-    entry: './src/index.js',
+  entry: "./src/index.js",
 
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        publicPath: '/build/',
-        filename: 'project.bundle.js'
-    },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/build/",
+    filename: "project.bundle.js",
+  },
 
-    devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: [/\.vert$/, /\.frag$/],
+        use: "raw-loader",
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg|xml)$/i,
+        use: "file-loader",
+      },
+    ],
+  },
 
-    module: {
-        rules: [
-          {
-            test: [ /\.vert$/, /\.frag$/ ],
-            use: 'raw-loader'
-          }
-        ]
-    },
-
-    plugins: [
-        new webpack.DefinePlugin({
-            'CANVAS_RENDERER': JSON.stringify(true),
-            'WEBGL_RENDERER': JSON.stringify(true)
-        })
-    ]
-
+  plugins: [
+    new CleanWebpackPlugin({
+      root: path.resolve(__dirname, "./")
+    }),
+    new webpack.DefinePlugin({
+      CANVAS_RENDERER: JSON.stringify(true),
+      WEBGL_RENDERER: JSON.stringify(true)
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    })
+  ],
 };
