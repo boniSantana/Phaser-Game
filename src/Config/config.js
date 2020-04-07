ab607d35620be7b1a041c3114a691ccb8b75b950
Federico Santana <fede.santana@hotmail.com>
2020-04-07 06:19:24 -0300

    Delete innecesary lines

--- src/Config/config.js
+++ src/Config/config.js
@@ -16,8 +16,7 @@ export default {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
     parent: "thegame",
-    width: 800,
-    height: 600,
+ 
   },
   physics: {
     default: "arcade",
